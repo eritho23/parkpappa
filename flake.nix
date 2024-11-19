6aa28b06bf38b79939404659dddfd7848479977c
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2024-11-19 10:04:27 +0100

    use flake nix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,33 @@
 {
-    inputs = {
-        # Unstable Nixpkgs
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    # Unstable Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-        # Flake utils
-        utils.url = "github:numtide/flake-utils";
-    };
+    # Flake utils
+    utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = {self, nixpkgs, ...}: {
-
-    };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      version = "0.0.0-dev";
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_22
+          nodePackages.npm
+          ripgrep
+          fzf
+          mdcat
+          bat
+          alejandra
+          nil
+        ];
+      };
+    });
 }
