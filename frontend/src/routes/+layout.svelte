a9d4db8ae0a316640af52158df3dce30175bab73
Adrian Damianovici <adrian.damianovici@hitachigymnasiet.se>
2024-11-19 10:30:11 +0100

    updated cookie and changed default colors

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -5,17 +5,17 @@ import { Navbar, NavBrand, NavHamburger, NavLi, NavUl} from "flowbite-svelte"
 
 
 <div class="relative px-8">
-  <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b">
+  <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b bg-background-foreground">
     <NavBrand href="/">
-      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+      <span class="self-center whitespace-nowrap text-xl font-semibold text-text dark:text-white">Parkpappa</span>
     </NavBrand>
     <NavHamburger />
     <NavUl>
-      <NavLi href="/">Home</NavLi>
-      <NavLi href="/about">About</NavLi>
-      <NavLi href="/docs/components/navbar">Navbar</NavLi>
-      <NavLi href="/pricing">Pricing</NavLi>
-      <NavLi href="/contact">Contact</NavLi>
+      <NavLi class="text-text" href="/">Home</NavLi>
+      <NavLi class="text-text" href="/about">About</NavLi>
+      <NavLi class="text-text" href="/docs/components/navbar">Navbar</NavLi>
+      <NavLi class="text-text" href="/pricing">Pricing</NavLi>
+      <NavLi class="text-text" href="/contact">Contact</NavLi>
     </NavUl>
   </Navbar>
   <slot></slot>
