87f8ca2ddd4906123b6f483645dceb661ebef629
Adrian Damianovici <adrian.damianovici@hitachigymnasiet.se>
2024-11-19 10:14:12 +0100

    added colors to tailwindcss and started on header

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
 import "../app.css"
+import { Navbar, NavBrand, NavHamburger, NavLi, NavUl} from "flowbite-svelte"
 </script>
 
 
-<div class="bg-red-500 size-10"></div>
+<div class="relative px-8">
+  <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b">
+    <NavBrand href="/">
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    </NavBrand>
+    <NavHamburger />
+    <NavUl>
+      <NavLi href="/">Home</NavLi>
+      <NavLi href="/about">About</NavLi>
+      <NavLi href="/docs/components/navbar">Navbar</NavLi>
+      <NavLi href="/pricing">Pricing</NavLi>
+      <NavLi href="/contact">Contact</NavLi>
+    </NavUl>
+  </Navbar>
+  <slot></slot>
+</div>
 
 
-<slot></slot>
+
+
+
