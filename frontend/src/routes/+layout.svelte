bffbb9049e9d53f402ab4168a379b13b9a6bd158
Adrian Damianovici <adrian.damianovici@hitachigymnasiet.se>
2024-11-19 10:54:30 +0100

    downloaded font to static

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
 import "../app.css"
 import { Navbar, NavBrand, NavHamburger, NavLi, NavUl} from "flowbite-svelte"
+import { page } from "$app/stores"
+$: activeUrl = $page.url.pathname;
+const activeClass = "text-primary bold underline";
+const nonActiveClass = "text-text hover:text-primary";
 </script>
 
 
 <div class="relative px-8">
   <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b bg-background-foreground">
     <NavBrand href="/">
-      <span class="self-center whitespace-nowrap text-xl font-semibold text-text dark:text-white">Parkpappa</span>
+      <span class="self-center whitespace-nowrap text-xl font-semibold text-primary dark:text-white">Parkpappa</span>
     </NavBrand>
     <NavHamburger />
-    <NavUl>
-      <NavLi class="text-text" href="/">Home</NavLi>
-      <NavLi class="text-text" href="/about">About</NavLi>
-      <NavLi class="text-text" href="/docs/components/navbar">Navbar</NavLi>
-      <NavLi class="text-text" href="/pricing">Pricing</NavLi>
-      <NavLi class="text-text" href="/contact">Contact</NavLi>
+    <NavUl {activeUrl} {activeClass} {nonActiveClass}>
+      <NavLi href="/">Hem</NavLi>
+      <NavLi href="/map">Karta</NavLi>
+      <NavLi href="/reviews">Recensioner</NavLi>
+      <NavLi href="/settings">Inställningar</NavLi>
+      <NavLi href="/about">Om oss</NavLi>
+      <NavLi href="/contact">Kontakta oss</NavLi>
     </NavUl>
   </Navbar>
   <slot></slot>
