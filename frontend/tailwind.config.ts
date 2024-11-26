7c9fb9f4cceeee6dd9e2d0a85170ca4bb39b4559
Adrian Damianovici <adrian.damianovici@hitachigymnasiet.se>
2024-11-26 09:55:38 +0100

    applied formattitng to files

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,42 +1,44 @@
-import { Config } from "tailwindcss/types/config";
-import flowbitePlugin from 'flowbite/plugin'
+import { Config } from 'tailwindcss/types/config';
+import flowbitePlugin from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
-  darkMode: 'selector',
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-         DEFAULT: "#E5102C"
+    content: [
+        './src/**/*.{html,js,svelte,ts}',
+        './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+    ],
+    darkMode: 'selector',
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#E5102C',
+                },
+                text: {
+                    DEFAULT: '#000000',
+                    light: '#ACACAC',
+                    dark: '#595858',
+                },
+                background: {
+                    DEFAULT: '#ECECEC',
+                    foreground: '#FFFFFF',
+                },
+                ring: {
+                    DEFAULT: '#DFDFDF',
+                },
+                gold: {
+                    DEFAULT: 'F2C00E',
+                },
+            },
+            fontWeight: {
+                normal: '400',
+                medium: '500',
+                semibold: '600',
+            },
         },
-        text: {
-          DEFAULT: "#000000",
-          light: "#ACACAC",
-          dark: "#595858"
+        fontFamily: {
+            primary: ['Lexend'],
         },
-        background: {
-          DEFAULT: "#ECECEC",
-          foreground: "#FFFFFF"
-        },
-        ring: {
-          DEFAULT: "#DFDFDF"
-        },
-        gold: {
-          DEFAULT: "F2C00E"
-        }
-      },
-      fontWeight: {
-        normal: "400",
-        medium: "500",
-        semibold: "600"
-      }
     },
-    fontFamily: {
-      "primary": ["Lexend"]
-    }
-  },
-  plugins: [flowbitePlugin],
+    plugins: [flowbitePlugin],
 } as Config;
-
