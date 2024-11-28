47dcb2d59f19b11112a05fd1d9b3dc0ba7b595d5
Adrian Damianovici <adrian.damianovici@hitachigymnasiet.se>
2024-11-28 13:25:34 +0100

    fixed window not defined and fixed height of map

--- frontend/src/lib/components/map.svelte
+++ frontend/src/lib/components/map.svelte
@@ -1,10 +1,10 @@
 <script>
-    import * as L from 'leaflet';
+   import * as L from 'leaflet';
     // If you're playing with this in the Svelte REPL, import the CSS using the
     // syntax in svelte:head instead. For normal development, this is better.
     import 'leaflet/dist/leaflet.css';
-    let map;
-  
+    import { onMount } from 'svelte';
+    let map;  
     function createMap(container) {
       let m = L.map(container).setView([59.609796, 16.546400], 14);
       L.tileLayer(
@@ -26,11 +26,14 @@
       map.setMaxBounds(L.latLngBounds(L.latLng(59.846050, 16.234630), L.latLng(59.452476, 16.965909)))            //[[59.846050, 16.234630], [59.452476, 16.965909]]
       return {
         destroy: () => {
-          map.remove();
+          if (map) map.remove();
         },
       };
     }
+
+
+    setTimeout(() => map.invalidateSize(), 0);
   </script>
   
   
-  <div class="w-full h-full" use:mapAction ></div>
+  <div class="w-full h-full flex-grow" use:mapAction></div>
