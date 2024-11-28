6309d843942dd111114ca780e76ed3683f2f10c4
Adrian Damianovici <adrian.damianovici@hitachigymnasiet.se>
2024-11-28 13:47:35 +0100

    added system for creating markers

--- frontend/src/lib/components/map.svelte
+++ frontend/src/lib/components/map.svelte
@@ -5,6 +5,8 @@
     import 'leaflet/dist/leaflet.css';
     import { onMount } from 'svelte';
     let map;  
+    let markerLayers;
+
     function createMap(container) {
       let m = L.map(container).setView([59.609796, 16.546400], 14);
       L.tileLayer(
@@ -16,20 +18,38 @@
           maxZoom: 18,
           minZoom: 12
         }
-      ).addTo(m);
-  
+      ).addTo(m);    
       return m;
     }
+
+    function createMarker(loc) {
+		let marker = L.marker(loc)
+		.addTo(map)
+		.bindPopup('popup text')
+		return marker;
+	}
+
   
     function mapAction(container) {
       map = createMap(container);
-      map.setMaxBounds(L.latLngBounds(L.latLng(59.846050, 16.234630), L.latLng(59.452476, 16.965909)))            //[[59.846050, 16.234630], [59.452476, 16.965909]]
+      map.setMaxBounds(L.latLngBounds(L.latLng(59.846050, 16.234630), L.latLng(59.452476, 16.965909)));
+      markerLayers = L.layerGroup();
+			markerLayers.addTo(map);
       return {
         destroy: () => {
           if (map) map.remove();
         },
       };
     }
+
+    $: if (map && markerLayers) {
+		try {
+				const marker = createMarker(L.latLng(59.6330795581567, 16.5470303778179));
+				markerLayers.addLayer(marker);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
 
     setTimeout(() => map.invalidateSize(), 0);
