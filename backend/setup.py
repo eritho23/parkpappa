54185e91d36ab87f0434edc36abf7db99ccc3e03
David <catidifat@gmail.com>
2024-12-18 10:39:54 +0100

    Start command fix

--- backend/setup.py
+++ backend/setup.py
@@ -14,8 +14,7 @@ setup(
     install_requires=read_requirements('requirements.txt'), 
     entry_points={
         'console_scripts': [
-            'run-parks-api=app.ParkApi:app',
-            
+            'run-parks-api=app.ParkApi:app.run',
         ],
     },
 )
