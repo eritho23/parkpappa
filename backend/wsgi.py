c18f975effc34d1e648cbfb357e5983694c860ba
David <catidifat@gmail.com>
2025-01-07 10:04:49 +0100

    Fixed import statement(maybe)

---
+++ backend/wsgi.py
@@ -0,0 +1,18 @@
+from app.ParkApi import app, scrape_data  # Import app and scrape_data from ParkApi.py
+import schedule
+import threading
+import time
+
+def schedule_scrape():
+    # Schedule the scrape_data function to run daily at midnight
+    schedule.every().day.at("00:00").do(scrape_data)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+# Start the scheduling thread
+threading.Thread(target=schedule_scrape, daemon=True).start()
+
+if __name__ == '__main__':
+    app.run()
