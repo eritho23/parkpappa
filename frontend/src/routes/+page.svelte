1e7fd2ccaba0b5ef997c6f17baad66c7efd4c8f4
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-01-09 11:08:46 +0100

    fix error

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,8 +4,6 @@
     import {Check, Map, SquarePlus, ShieldCheck, MapPinCheckInside} from 'lucide-svelte';
 </script>
 
-
-</script>
 <ParkInfo></ParkInfo>
 <div class="flex flex-col overflow-y-scroll h-screen">
     <div class="text-center w-full h-full px-auto justify-center bg-white border-b border-grey/90 flex flex-col space-y-8 py-12">
