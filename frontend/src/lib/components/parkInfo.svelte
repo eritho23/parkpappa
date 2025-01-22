f6333a6bf2191fb9365e398518d4c2ec3b9c4e1b
PokenDev21 <110134664+PokenDev21@users.noreply.github.com>
2025-01-22 18:54:14 +0100

    Fixed issue for text spacing

--- frontend/src/lib/components/parkInfo.svelte
+++ frontend/src/lib/components/parkInfo.svelte
@@ -110,7 +110,10 @@
         src="./placeholders/playground.jpg"
         alt="Playground"
     />
+    {:else}
+    <div class="ml-2 pb-4"></div>
     {/if}
+    
     <div class="ml-2 pb-4">
         <div>
             <h1 class="md:text-xl lg:text-2xl">{parkData?.Name}</h1>
