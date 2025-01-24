d4f296c9018bd54f1304607880c5cf7a426435b9
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-01-24 12:19:44 +0100

    some kind of auth + formatting

--- frontend/src/routes/map/+page.server.ts
+++ frontend/src/routes/map/+page.server.ts
@@ -2,7 +2,7 @@ import { API_PATH } from '$env/static/private';
 import { env } from '$env/dynamic/private';
 const {GOOGLE_MAPS_API_KEY} = env;
 
-export const load = async ({ params, url, locals }) => {
+export const load = async ({ url, locals }) => {
     let goToPark = url.searchParams.get('park') ?? null;
     const isLoggedIn = locals.pb.authStore.isValid;
     return {
