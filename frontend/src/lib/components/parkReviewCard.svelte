7b1ce68523bdcdb8043a136f244a0bd4447f7f45
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-01-25 20:46:00 +0100

    review page removed entirely

--- frontend/src/lib/components/parkReviewCard.svelte
+++ frontend/src/lib/components/parkReviewCard.svelte
@@ -4,14 +4,16 @@
         name: string,
         stars: number,
         reviewText: string,
-        playgroundId: number
+        playgroundId: number,
+        title: string
     };
 
-    let {name, stars, reviewText, playgroundId}: Props = $props();
+    let {name, stars, reviewText, playgroundId, title}: Props = $props();
 </script>
 
 <div class="border-primary/20 border-2 rounded p-4">
-    <h1 class="font-bold text-2xl">{name}</h1>
+    <h1 class="font-bold text-2xl">{title}</h1>
+    <span>{name}</span>
     <StarRating rating={stars * 2} ></StarRating>
     <p>{reviewText}</p>
     <a href="/map?park={playgroundId}" class="text-primary underline">Gå till park</a>
