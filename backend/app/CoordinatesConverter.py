4ca581644c9b193987e3d0b7f224d4b7990b430f
PokenDev21 <110134664+PokenDev21@users.noreply.github.com>
2025-01-26 12:29:20 +0100

    Outdated syntax fix

--- backend/app/CoordinatesConverter.py
+++ backend/app/CoordinatesConverter.py
@@ -1,8 +1,8 @@
 from pyproj import Proj, transform
 
 # SWEREF 99 16 30 to WGS84
-sweref99_16_30 = Proj(init='epsg:3010')  # SWEREF 99 16 30
-wgs84 = Proj(init='epsg:4326')  # WGS84
+sweref99_16_30 = Proj('epsg:3010')  # SWEREF 99 16 30
+wgs84 = Proj('epsg:4326')  # WGS84
 
 def ConvertToWGS(easting, northing):
     longitude, latitude = transform(sweref99_16_30, wgs84, easting, northing)
